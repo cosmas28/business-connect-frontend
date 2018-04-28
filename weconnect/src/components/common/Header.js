9c8261cbdfb37325a7b3b95c6d06946d4438ef62
cosmas28 <augubilla100@gmail.com>
2018-04-28 10:39:20 +0300

    Add home route

--- weconnect/src/components/common/Header.js
+++ weconnect/src/components/common/Header.js
@@ -15,7 +15,7 @@ const Header = () => <header>
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item"><a className="nav-link" href="">Business<span
                             className="sr-only">(current)</span></a></li>
-                        <li className="nav-item"><a className="nav-link" href="">Sign Up</a></li>
+                        <li className="nav-item"><a className="nav-link" href="/">Sign Up</a></li>
                         <li className="nav-item"><a className="nav-link" href="">Login</a></li>
                     </ul>
                 </div>
