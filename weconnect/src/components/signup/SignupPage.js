2e2229ea72ef205ee708503e12da999484e0234c
cosmas28 <augubilla100@gmail.com>
2018-04-28 15:43:28 +0300

    Add submit form handler

--- weconnect/src/components/signup/SignupPage.js
+++ weconnect/src/components/signup/SignupPage.js
@@ -8,10 +8,25 @@ class SignUpPage extends React.Component {
         super(props);
         this.state = { 'message': '' };
         this.apiUrl = 'https://weconnect-v2.herokuapp.com/api/v2/auth/register';
-        this.registerUser = this.registerUser.bind(this);
     }
 
-    registerUser(input) {
+    handleInputChange = event => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({ [name]: value });
+    };
+
+    newUserSubmitHandler = event => {
+        event.preventDefault();
+        const input = {
+            'confirm_password': this.state.confirm_password,
+            'email': this.state.email,
+            'first_name': this.state.first_name,
+            'last_name': this.state.last_name,
+            'password': this.state.password,
+            'username': this.state.username
+        };
         axios.post(this.apiUrl, input).then(response => {
             if (response.status === 201) {
                 this.setState({ 'message': response.data.message });
@@ -19,7 +34,7 @@ class SignUpPage extends React.Component {
                 this.setState({ 'message': response.data.message });
             }
         });
-    }
+    };
 
     render() {
         return (
@@ -27,7 +42,11 @@ class SignUpPage extends React.Component {
                 <div className="container">
                     <div className="row">
                         <Home/>
-                        <SignUpForm registerUser={this.registerUser} message={this.state.message}/>
+                        <SignUpForm
+                            handleInputChange={this.handleInputChange}
+                            handleSubmitForm={this.newUserSubmitHandler}
+                            outPutMessage={this.state.message}
+                        />
                     </div>
                 </div>
             </main>
