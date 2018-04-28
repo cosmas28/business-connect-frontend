2ab95f03152844d45bf277ec2a254f8cf2c2e4fa
cosmas28 <augubilla100@gmail.com>
2018-04-28 15:42:30 +0300

    Change into snapshot test specs

--- weconnect/src/components/signup/SignupPage.test.js
+++ weconnect/src/components/signup/SignupPage.test.js
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom';
 import SignUpPage from './SignupPage';
 
 describe('Sign up Page', () => {
+    const mockRegisterUser = jest.fn();
+    const component = shallow(
+        <SignUpPage />
+        );
     it('renders signup page without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<SignUpPage />, div);
-        ReactDOM.unmountComponentAtNode(div);
+        expect(component).toMatchSnapshot();
     });
 });
