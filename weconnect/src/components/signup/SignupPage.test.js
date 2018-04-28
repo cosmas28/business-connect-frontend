f58cc519dd00990cd13a451939de5e00c9f729ff
cosmas28 <augubilla100@gmail.com>
2018-04-28 10:46:43 +0300

    Edit test specs organization

--- weconnect/src/components/signup/SignupPage.test.js
+++ weconnect/src/components/signup/SignupPage.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
+// import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
 import SignUpPage from './SignupPage';
 
-it('renders signup page without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<SignUpPage />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('Sign up Page', () => {
+    it('renders signup page without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<SignUpPage />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
