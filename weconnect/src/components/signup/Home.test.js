952063312ac852ee3c32fce74574af4d3609d0b6
cosmas28 <augubilla100@gmail.com>
2018-04-28 10:40:42 +0300

    Add snapshot

--- weconnect/src/components/signup/Home.test.js
+++ weconnect/src/components/signup/Home.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home';
 
@@ -5,4 +6,9 @@ it('renders home page text without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Home />, div);
     ReactDOM.unmountComponentAtNode(div);
+});
+
+test('render home page', () => {
+    const wrapper = shallow(<Home />);
+    expect(wrapper).toMatchSnapshot();
 });
