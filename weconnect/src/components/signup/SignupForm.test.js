c31d5430ca5ceb25f6303243b086c4ebcf79cee3
cosmas28 <augubilla100@gmail.com>
2018-04-28 10:43:58 +0300

    Add event test specs

--- weconnect/src/components/signup/SignupForm.test.js
+++ weconnect/src/components/signup/SignupForm.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
 import ReactDOM from 'react-dom';
 import SignUpForm from './SignupForm';
 
-it('renders home page text without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<SignUpForm />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('Sign up form', () => {
+    const mockRegisterUser = jest.fn();
+    const component = shallow(
+        <SignUpForm registerUser={mockRegisterUser} />
+    ),
+        userInfo = {
+            'confirm_password': 'andela2018',
+            'email': 'cosmas@andela.com',
+            'first_name': 'augustino',
+            'last_name': 'billa',
+            'password': 'andela2018',
+            'username': 'cosmas'
+        };
+
+    it('renders and matches our snapshot', () => {
+        expect(component).toMatchSnapshot();
+    });
+
+    it('contains the form', () => {
+        expect(component.find('input')).toHaveLength(6);
+        expect(component.find('button')).toHaveLength(1);
+    });
+
+    it('should call registerUser when submit button is called', () => {
+        component.find('form').simulate('submit', { preventDefault: () => {} });
+        expect(mockRegisterUser).toBeCalled();
+    });
+
+    it('should register call registerUser based on the value in the state', () => {
+        component.setState({ userInfo });
+        component.find('form').simulate('submit', { preventDefault: () => {} });
+        expect(mockRegisterUser).toBeCalled();
+    });
 });
