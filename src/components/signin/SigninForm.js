16ca12daf33765fd5ee9f25287a9a7b47796bd32
cosmas28 <augubilla100@gmail.com>
2018-04-29 18:27:00 +0300

    Add onSubmit and onChange event handlers [#157075126]

--- src/components/signin/SigninForm.js
+++ src/components/signin/SigninForm.js
@@ -5,12 +5,25 @@ const SignInForm = props => {
     return (
         <div className="main-login-page">
             <h2>Sign in to your account</h2>
-            <form>
+            <form onSubmit={props.handleLoginSubmitForm}>
                 <div className="form-input-division">
-                    <input type="text" id="username" name="username" className="input-default form-text-input" placeholder="Username" />
+                    <input type="text"
+                           name="username"
+                           className="input-default form-text-input"
+                           placeholder="Username"
+                           onChange={props.handleLoginInputChange}
+                           required
+                    />
                 </div>
                 <div className="form-input-division">
-                    <input type="password" id="password" name="password" className="input-default form-text-input" placeholder="Password" />
+                    <input type="password"
+                           id="password"
+                           name="password"
+                           className="test input-default form-text-input"
+                           placeholder="Password"
+                           onChange={props.handleLoginInputChange}
+                           required
+                    />
                 </div>
                 <div className="form-input-division">
                     <button type="submit" className="btn btn-primary input-default form-btn">Sign In</button>
@@ -25,6 +38,11 @@ const SignInForm = props => {
             </form>
         </div>
     );
+};
+
+SignInForm.propTypes = {
+    'handleLoginInputChange': PropTypes.func.isRequired,
+    'handleLoginSubmitForm': PropTypes.func.isRequired
 };
 
 export default SignInForm;
