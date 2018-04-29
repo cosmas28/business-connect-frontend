995531805f19ebfda484c008493c5b8e3d4cda4e
cosmas28 <augubilla100@gmail.com>
2018-04-29 22:55:20 +0300

    Rename to SignInPage [#157075126]

--- src/components/signin/SigninPage.js
+++ src/components/signin/SigninPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const SignInForm = props => {
+const SignInPage = props => {
     let responseMessage = null;
     if (props.loginSuccessMessage) {
         responseMessage = <div className="alert alert-success" role="alert">
@@ -18,49 +18,57 @@ const SignInForm = props => {
     }
 
     return (
-        <div className="main-login-page">
-            <h2>Sign in to your account</h2>
-            {responseMessage}
-            <form onSubmit={props.handleLoginSubmitForm}>
-                <div className="form-input-division">
-                    <input type="email"
-                           name="email"
-                           className="input-default form-text-input"
-                           placeholder="Email"
-                           onChange={props.handleLoginInputChange}
-                           required
-                    />
-                </div>
-                <div className="form-input-division">
-                    <input type="password"
-                           id="password"
-                           name="password"
-                           className="test input-default form-text-input"
-                           placeholder="Password"
-                           onChange={props.handleLoginInputChange}
-                           required
-                    />
-                </div>
-                <div className="form-input-division">
-                    <button type="submit" className="btn btn-primary input-default form-btn">Sign In</button>
-                </div>
-                <div className="form-input-division">
-                    <Link className="nav-link" to="/reset">Forgot password?</Link>
-                </div>
-                <hr />
-                    <div className="form-input-division">
-                        <Link className="nav-link" to="/">You don't have an account?</Link>
+        <main className="main-content">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 col-sm-12 com-xs-12">
+                        <div className="main-login-page">
+                            <h2>Sign in to your account</h2>
+                            {responseMessage}
+                            <form onSubmit={props.handleLoginSubmitForm}>
+                                <div className="form-input-division">
+                                    <input type="email"
+                                           name="email"
+                                           className="input-default form-text-input"
+                                           placeholder="Email"
+                                           onChange={props.handleLoginInputChange}
+                                           required
+                                    />
+                                </div>
+                                <div className="form-input-division">
+                                    <input type="password"
+                                           id="password"
+                                           name="password"
+                                           className="test input-default form-text-input"
+                                           placeholder="Password"
+                                           onChange={props.handleLoginInputChange}
+                                           required
+                                    />
+                                </div>
+                                <div className="form-input-division">
+                                    <button type="submit" className="btn btn-primary input-default form-btn">Sign In</button>
+                                </div>
+                                <div className="form-input-division">
+                                    <Link className="nav-link" to="/reset">Forgot password?</Link>
+                                </div>
+                                <hr />
+                                <div className="form-input-division">
+                                    <Link className="nav-link" to="/">You don't have an account?</Link>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-            </form>
-        </div>
+                </div>
+            </div>
+        </main>
     );
 };
 
-SignInForm.propTypes = {
+SignInPage.propTypes = {
     'handleLoginInputChange': PropTypes.func.isRequired,
     'handleLoginSubmitForm': PropTypes.func.isRequired,
     'loginPutErrorMessage': PropTypes.string,
     'loginSuccessMessage': PropTypes.string
 };
 
-export default SignInForm;
+export default SignInPage;
