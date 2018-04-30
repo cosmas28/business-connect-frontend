65fc4367a60496426d586db69a30c36d897af6b1
cosmas28 <augubilla100@gmail.com>
2018-04-30 15:43:43 +0300

    Implement minimal rendering

---
+++ src/components/resetPassword/ResetPasswordPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ResetPasswordPage extends React.Component {
+    render(){
+        return (
+            <main className="main-content">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 col-sm-12 col-xs-12">
+
+                        </div>
+                    </div>
+                </div>
+            </main>
+        );
+    }
+}
+
+export default ResetPasswordPage;
