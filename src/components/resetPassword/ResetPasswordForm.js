1a79d0a7949357b21d80caa8e30f0313d3a69960
cosmas28 <augubilla100@gmail.com>
2018-04-30 15:46:42 +0300

    Implement rendering and event handlers callbacks [#157185874]

---
+++ src/components/resetPassword/ResetPasswordForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ResetPasswordForm = props => {
+    let responseMessage = null;
+    if (props.ResetPasswordSuccessMessage) {
+        responseMessage = <div className="alert alert-success" role="alert">
+            {props.ResetPasswordSuccessMessage}
+        </div>;
+    } else if (props.ResetPasswordPutErrorMessage) {
+        responseMessage = <div className="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error</strong> {props.ResetPasswordPutErrorMessage}
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>;
+    }
+
+    return (
+        <div className="main-login-page">
+            <h2>Reset your password</h2>
+            {responseMessage}
+            <form onSubmit={props.handleResetPasswordSubmitForm}>
+                <div className="form-input-division">
+                    <input type="email"
+                           name="email"
+                           className="test input-default form-text-input"
+                           placeholder="Email"
+                           onChange={props.handleResetPasswordInputChange}
+                           required
+                    />
+                </div>
+                <div className="form-input-division">
+                    <input type="password"
+                           id="password"
+                           name="password"
+                           className="input-default form-text-input" placeholder="New password"
+                           onChange={props.handleResetPasswordInputChange}
+                           required
+                    />
+                </div>
+                <div className="form-input-division">
+                    <input type="password" id="confirm_pwd" name="confirm_pwd" className="input-default form-text-input" placeholder="Confirm password" />
+                </div>
+                <div className="form-input-division">
+                    <button type="submit" className="btn btn-primary input-default form-btn">Reset password</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+ResetPasswordForm.protoTypes = {
+    'ResetPasswordPutErrorMessage': PropTypes.string,
+    'ResetPasswordSuccessMessage': PropTypes.string,
+    'handleResetPasswordInputChange': PropTypes.func.isRequired,
+    'handleResetPasswordSubmitForm': PropTypes.func.isRequired
+};
+
+export default ResetPasswordForm;
