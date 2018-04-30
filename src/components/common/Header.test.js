dd915cfb97f465c46845c38147059bab8553c9bd
cosmas28 <augubilla100@gmail.com>
2018-04-30 08:04:29 +0300

    Fixes Link testing errors [Finishes #157178165]

--- src/components/common/Header.test.js
+++ src/components/common/Header.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter } from 'react-router-dom';
+
 import Header from './Header';
 
 it('renders header without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Header />, div);
+    ReactDOM.render(
+        <MemoryRouter><Header /></MemoryRouter>,
+        div);
     ReactDOM.unmountComponentAtNode(div);
 });
