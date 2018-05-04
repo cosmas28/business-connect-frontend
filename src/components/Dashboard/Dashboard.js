30718a0589574f67cdb876b5037d55ca061adc8f
cosmas28 <augubilla100@gmail.com>
2018-05-04 16:13:49 +0300

    Add handleInputChange method [#157075451]

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -4,8 +4,24 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 
 import DashboardNavBar from './../common/DashboardNavBar';
 import UserBusinessView from './UserBusinessView';
+import AddBusinessForm from './AddBusinessForm'
 
 class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'errorMessage': '',
+            'successMessage': ''
+        };
+        this.apiUrl = 'https://weconnect-v2.herokuapp.com/api/v2/businesses';
+    }
+
+    handleInputChange = event => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({ [name]: value });
+    };
     render() {
         return (
             <div className="page-wrapper">
