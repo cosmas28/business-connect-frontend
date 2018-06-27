535e66197c1d84b47f4896c30939570ca684ca2d
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-06-27 07:39:11 +0300

    provide store to the entry point component using Provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap.min.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from './store/configureStore.dev';
+
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
