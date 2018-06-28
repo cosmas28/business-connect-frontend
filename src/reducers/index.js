50d05ab9c66ca418c4c170692f814d86ee10f8b5
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-06-28 06:04:03 +0300

    add logi reducers to the root reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { registerUserReducer } from './registerUserReducers';
+import { loginReducer } from './loginReducers';
 
 export default combineReducers({
-    user: registerUserReducer
+    user: registerUserReducer,
+    login: loginReducer
 })
