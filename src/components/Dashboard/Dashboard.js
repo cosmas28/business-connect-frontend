48ad78f9443d00f078823836e7633a6e2e0eedd9
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-06-28 13:07:03 +0300

    feat: implement view all business in dashboard
    - connect Dashboard to the store
    - ensures all registered businesses can be viewd by logged in users
    
    [#158593229]

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,31 +1,71 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Route, Redirect, Switch } from 'react-router-dom';
-import axios from "axios";
+import { connect } from 'react-redux';
 
 import DashboardNavBar from './../common/DashboardNavBar';
-import UserBusinessView from './UserBusinessView';
+import DashboardTitle from './../common/DashboardTitle';
+import { fetchBusinesses } from '../../actions/businessActions';
 
 class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.accessToken = sessionStorage.getItem('accessToken');
-        this.apiUrl = 'https://weconnect-v2.herokuapp.com/api/v2/businesses';
+    }
+
+    componentDidMount() {
+        this.props.fetchBusinesses(this.accessToken);
     }
 
     render() {
         return (
             <div className="page-wrapper">
                 <DashboardNavBar/>
-                <Route exact path="/dashboard" render={
-                    () => <Redirect to={{
-                        'pathname': '/dashboard/user_business'
-                    }}/>
-                } />
+                <main className="main-body">
+                    <div className="container-fluid">
+                        <DashboardTitle title="Registered Business List"/>
+                        <div className="row no-gutters">
+                            <div className="col-md-12 col-sm-12 col-xs-12">
+                                <div className="main-view-page">
+                                {this.props.businesses.map((business, id) => {
+                                    return (
+                                        <div className="row no-gutters business-view">
+                                            <div className="col-md-12 col-sm-12 col-xs-12">
+                                                <div className="view-right-side">
+                                                    <h3 className="">{business.name}</h3>
+                                                    <div>
+                                                        <span className="badge badge-primary">{business.location}</span>
+                                                        <span className="badge badge-info">{business.category}</span>
+                                                    </div>
+                                                    <p>
+                                                    {business.summary}
+                                                    </p>
+                                                    <a className="btn btn-primary" href="single-business.html">
+                                                        <span className="badge badge-light">4</span>Reviews
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
             </div>
         );
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        businesses: state.businesses
+    }
+};
 
-export default Dashboard;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        fetchBusinesses: accessToken => dispatch(fetchBusinesses(accessToken))
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
