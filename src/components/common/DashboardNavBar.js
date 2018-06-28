1f6ccf673ac41c03f9de81564602437ae48815e5
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-06-28 12:49:45 +0300

    add a line to set the loggedIn key to false when user logout

--- src/components/common/DashboardNavBar.js
+++ src/components/common/DashboardNavBar.js
@@ -14,6 +14,7 @@ class DashboardNavBar extends React.Component {
 
     onClickLogout  = event => {
         sessionStorage.removeItem('accessToken');
+        sessionStorage.setItem('loggedIn', false);
         sessionStorage.removeItem('userId');
         axios({ method: 'post', url: this.logoutUrl });
     }
