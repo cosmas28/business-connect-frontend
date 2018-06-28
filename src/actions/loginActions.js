31ca2df5cac6ca752369766a0b2d59b1217856b8
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-06-28 05:59:41 +0300

    develop login action creater

---
+++ src/actions/loginActions.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import * as actionTypes from './actionTypes';
+import history from './../helpers/history';
+
+const apiUrl = 'https://weconnect-v2.herokuapp.com/api/v2/auth/login';
+
+export const loginFailed = (loginError) => {
+    return {
+        type: actionTypes.LOGIN_FAILED,
+        loginError
+    }
+};
+
+export const loginSuccess = (loginResponse) => {
+    return {
+        type: actionTypes.LOGIN_SUCCESS,
+        loginResponse
+    }
+};
+
+export const doLogin = (loginInput) => {
+    return (dispatch) => {
+        return axios.post(apiUrl, loginInput)
+        .then(response => {
+            if (response.data.status_code === 200){
+                sessionStorage.setItem('loggedIn', true);
+                dispatch(loginSuccess(response.data))
+                history.replace('/dashboard');
+            } else {
+                dispatch(loginFailed(response.data))
+                history.replace('/login');
+            }
+        })
+        .catch(error => {
+            if (error.response) {
+                dispatch(loginFailed(error.response.data))
+                history.replace('/login');
+            }
+        });
+    };
+};
