ac7c0a1cc61d309fc8816d979e70a77714de9405
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-06-28 12:39:12 +0300

    add Switch to enable easy navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
-import axios from "axios";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import SignUpPage from './signup/SignupPage';
 import SignInPage from './signin/SigninPage';
@@ -14,19 +13,20 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.loggedIn = sessionStorage.getItem('loggedIn');
-        this.apiUrl = 'https://weconnect-v2.herokuapp.com/api/v2/businesses';
     }
 
     render() {
         return (
             <BrowserRouter>
                 <div className="page-wrapper">
-                    <Route exact path="/" component={SignUpPage}/>
-                    <Route exact path="/login" component={SignInPage}/>
-                    <Route exact path="/reset_password" component={ResetPasswordPage}/>
-                    <ProtectedRoute path="/dashboard" loggedIn={this.loggedIn} component={Dashboard} />
-                    <Route path="/dashboard/user_business" component={UserBusinessView} />
-                    <Route path="/dashboard/register_business" component={AddBusiness} />
+                    <Switch>
+                        <Route exact path="/" component={SignUpPage}/>
+                        <Route exact path="/login" component={SignInPage}/>
+                        <Route exact path="/reset_password" component={ResetPasswordPage}/>
+                        <ProtectedRoute path="/dashboard" loggedIn={this.loggedIn} component={Dashboard} />
+                        <Route path="/dashboard/user_business" component={UserBusinessView} />
+                        <Route path="/dashboard/register_business" component={AddBusiness} />
+                    </Switch>
                 </div>
             </BrowserRouter>
         );
