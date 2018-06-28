88a2e2d337a1e22581672a005c76617e1242e5fb
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-06-28 05:58:44 +0300

    add login action types

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,3 +1,9 @@
 // action types for signup componet
 export const REGISTER_USER_SUCCESS = 'REGISTER_USER_SUCCESS';
 export const REGISTER_USER_FAILED = 'REGISTER_USER_FAILED';
+
+// action types for login componet
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILED = 'LOGIN_FAILED';
+
+export const CLEAR_ERROR = 'CLEAR_ERROR';
