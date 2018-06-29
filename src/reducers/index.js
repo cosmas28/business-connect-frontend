7e3b285cc23a8d8d5360dc9cd5d9437459b027b1
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-06-29 11:34:47 +0300

    add user business reducer to the root reducer [#158593229]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import { registerUserReducer } from './registerUserReducers';
 import { loginReducer } from './loginReducers';
-import { businessesReducer } from './businessesReducers';
+import { businessesReducer, userBusinessReducer } from './businessesReducers';
 
 export default combineReducers({
     user: registerUserReducer,
     login: loginReducer,
-    businesses: businessesReducer
-})
+    businesses: businessesReducer,
+    userBusinesses: userBusinessReducer,
+});
