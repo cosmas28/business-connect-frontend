eba02e6d7e405c44a86129cbc3c043e365999bd8
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-03 15:02:26 +0300

    add confirm email reducer to the root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { registerUserReducer } from './registerUserReducers';
 import { loginReducer } from './loginReducers';
 import { businessesReducer, userBusinessReducer, registerBusinessReducer } from './businessesReducers';
+import { confirmEmailReducer } from './resetPasswordReducers';
 
 export default combineReducers({
     businesses: businessesReducer,
+    mail: confirmEmailReducer,
     login: loginReducer,
     registerBusiness: registerBusinessReducer,
     user: registerUserReducer,
