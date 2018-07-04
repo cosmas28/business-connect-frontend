078834d76125a93a78a07909a850e4dc492cd569
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-04 22:57:43 +0300

    change UI and display logic

--- src/components/Dashboard/ViewBusiness/OneBusinessView.js
+++ src/components/Dashboard/ViewBusiness/OneBusinessView.js
@@ -1,49 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const OneBusinessView = props => {
-    let renderedBusiness = null;
-    if (!props.errorMessage) {
-        renderedBusiness = <div className="row no-gutters">
-            <div className="col-md-5">
-                <div className="view-left-side">
-                    <a href="">
-                        <img className="view-image" src="http://placehold.it/700x300" alt="" />
+    return (
+        <div className="row no-gutters business-view">
+            <div className="col-md-12 col-sm-12 col-xs-12">
+                <div className="view-right-side">
+                <Link to={`/businesses/${props.id}`}><h3 className="">{props.name}</h3></Link>
+                    <div>
+                        <span className="badge badge-primary">{props.location}</span>
+                        <span className="badge badge-info">{props.category}</span>
+                    </div>
+                    <p>
+                    {props.summary}
+                    </p>
+                    <a className="btn btn-primary" href="single-business.html">
+                        <span className="badge badge-light">4</span>Reviews
                     </a>
                 </div>
             </div>
-            <div className="col-md-7">
-                <div className="view-right-side">
-                    <h3 className="business-name">{props.businessName}</h3>
-                    <h4>{props.businessOwner}</h4>
-                    <p className="business-summary">{props.businessSummary}</p>
-                    <div>
-                        <i className="business-location">{props.businessLocation}</i>-
-                        <strong className="business-category">{props.businessCategory}</strong>
-                    </div>
-                </div>
-            </div>
-        </div>;
-    } else if (props.errorMessage) {
-        renderedBusiness = <h3 className="error">{props.errorMessage}</h3>;
-    } else {
-        renderedBusiness = <p>You have not registered a business.Please register one.</p>;
-    }
-
-    return (
-        <div className="single-business">
-            {renderedBusiness}
         </div>
     );
 };
 
 OneBusinessView.propTypes = {
-    'businessCategory': PropTypes.string,
-    'businessLocation': PropTypes.string,
-    'businessName': PropTypes.string,
-    'businessOwner': PropTypes.string,
-    'businessSummary': PropTypes.string,
-    'errorMessage': PropTypes.string
+    'category': PropTypes.string,
+    'id': PropTypes.number,
+    'location': PropTypes.string,
+    'name': PropTypes.string,
+    'summary': PropTypes.string
 };
 
 export default OneBusinessView;
