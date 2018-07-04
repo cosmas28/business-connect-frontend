6f760ddfe2f88d4999c21eff4a3540c74bcbd123
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-04 23:01:31 +0300

    add businessReducer to the root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,16 @@
 import { combineReducers } from 'redux';
 import { registerUserReducer } from './registerUserReducers';
 import { loginReducer } from './loginReducers';
-import { businessesReducer, userBusinessReducer, registerBusinessReducer } from './businessesReducers';
+import {
+    businessesReducer,
+    userBusinessReducer,
+    registerBusinessReducer,
+    businessReducer
+} from './businessesReducers';
 import { confirmEmailReducer, resetPasswordReducer } from './resetPasswordReducers';
 
 export default combineReducers({
+    business: businessReducer,
     businesses: businessesReducer,
     login: loginReducer,
     mail: confirmEmailReducer,
