10d57901c7913509becda2908bbbf48d986acf89
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-04 09:43:41 +0300

    add reset password reducer to the root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,13 +2,14 @@ import { combineReducers } from 'redux';
 import { registerUserReducer } from './registerUserReducers';
 import { loginReducer } from './loginReducers';
 import { businessesReducer, userBusinessReducer, registerBusinessReducer } from './businessesReducers';
-import { confirmEmailReducer } from './resetPasswordReducers';
+import { confirmEmailReducer, resetPasswordReducer } from './resetPasswordReducers';
 
 export default combineReducers({
     businesses: businessesReducer,
-    mail: confirmEmailReducer,
     login: loginReducer,
+    mail: confirmEmailReducer,
     registerBusiness: registerBusinessReducer,
+    resetPassword: resetPasswordReducer,
     user: registerUserReducer,
     userBusinesses: userBusinessReducer
 });
