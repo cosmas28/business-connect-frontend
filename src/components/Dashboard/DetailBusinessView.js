9e97d2458ee2ba552d3f67bb3f124d75d0fd08e2
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-05 18:11:42 +0300

    update UI for displaying single business

--- src/components/Dashboard/DetailBusinessView.js
+++ src/components/Dashboard/DetailBusinessView.js
@@ -24,32 +24,56 @@ class DetailBusinessView extends React.Component {
                 <main className="main-body">
                     <div className="container-fluid">
                         <DashboardTitle title={this.props.business.name}/>
-                         <div className="row no-gutters">
-                            <div className="col-md-12 col-sm-12 col-xs-12">
-                                <div className="main-view-page">
-                                    <div className="single-business">
-                                        <div className="row no-gutters">
-                                            <div className="col-md-5">
-                                                <div className="view-left-side">
-                                                    <a href="">
-                                                        <img className="view-image" src="http://placehold.it/700x300" alt="" />
-                                                    </a>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-7">
-                                                <div className="view-right-side">
-                                                    <h3 className="business-name">{this.props.business.name}</h3>
-                                                    <h4>{this.props.business.owner}</h4>
-                                                    <p className="business-summary">{this.props.business.summary}</p>
-                                                    <div>
-                                                        <i className="business-location">{this.props.business.location}</i>-
-                                                        <strong className="business-category">{this.props.business.category}</strong>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                        <div className="row no-gutters">
+                            <div className="col-md-12">
+                                <div className="view-right-side">
+                                    <div>
+                                        <span className="badge badge-info">{this.props.business.category}</span>
+                                        <span className="business-text"><span className="fa fa-map-marker"></span> {this.props.business.location}</span> &bullet;
+                                        <span className="business-text"><span className="fa fa-comments"></span> 4</span> &bullet;
+                                        <span>By <span className="business-text">Business owner</span></span>
                                     </div>
+                                    <p>
+                                        {this.props.business.summary}
+                                    </p>
                                 </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12 col-sm-12 col-xs-12">
+                                <h3 className="r-comment">4 Comments</h3>
+                                <div className="comment-section">
+                                    <h5 className="r-username"><strong>Username</strong></h5>
+                                    <div>
+                                        <span className="business-text">July 9, 2018 AT 18:21</span>
+                                    </div>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                        Laudantium veniam exercitationem expedita laborum at
+                                        voluptate. Labore, voluptates totam at aut nemo deserunt
+                                        rem magni pariatur quos perspiciatis atque eveniet unde
+                                    </p>
+                                </div>
+                                <div className="comment-section">
+                                        <h5 className="r-username"><strong>Username</strong></h5>
+                                        <div>
+                                            <span className="business-text">July 9, 2018 AT 18:21</span>
+                                        </div>
+                                        <p>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Laudantium veniam exercitationem expedita laborum at
+                                            voluptate. Labore, voluptates totam at aut nemo deserunt
+                                            rem magni pariatur quos perspiciatis atque eveniet unde
+                                        </p>
+                                    </div>
+                                <form action="" method="post">
+                                    <div className="form-input-division">
+                                        <textarea name="business-review" id="business-review" className="register-form-input" placeholder="Write a review" rows="2"></textarea>
+                                    </div>
+                                    <div className="form-input-division">
+                                        <button type="submit" className="btn btn-primary">Post review</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
