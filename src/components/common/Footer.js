58a15e0f1f9cd10c32b04307844f137a82f64be3
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-05 18:16:08 +0300

    edit footer component UI to look more appealing

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const Footer = () => <footer className="fixed-bottom main-footer">
-        <div className="container-fluid">
-            <h5>2018 WeConnect</h5>
+const Footer = () => <footer className="site-footer">
+        <div className="container">
+        <div class="row">
+            <div class="col-md-12">
+                <i class="fa fa-briefcase" aria-hidden="true"></i> <a href="#" target="_blank">Copyright &copy;<script>document.write(new Date().getFullYear());</script></a>
+            </div>
+        </div>
         </div>
     </footer>;
 
