b967d3735d737e2eccaa5313387da0c09f81b80d
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-15 20:09:32 +0300

    design snapshot test for detail business presental component

---
+++ src/components/Dashboard/DetailBusinessComponent.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import DetailBusinessComponent from './DetailBusinessComponent';
+
+describe('One business view test suite', () => {
+    let mountedDetailBusinessComponent = null;
+    let props = null;
+    const component = () => {
+        if (!mountedDetailBusinessComponent) {
+            mountedDetailBusinessComponent = mount(
+                <MemoryRouter><DetailBusinessComponent {...props} /></MemoryRouter>
+            );
+        }
+
+        return mountedDetailBusinessComponent;
+    };
+
+    beforeEach(() => {
+        props = {
+            'category': null,
+            'location': null,
+            'name': null,
+            'summary': null
+        };
+        mountedDetailBusinessComponent = null;
+    });
+
+    it('renders without crashing', () => {
+        expect(component()).toHaveLength(1);
+    });
+});
