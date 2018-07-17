36b8f46567361dff91a7b835753a0a18f6252b19
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-17 11:53:22 +0300

    feat: render delete button and delete action response on DetailBusinessComponent
    - ensures a user recieve a proper response after clicking the delete button
    
    [#156530746]

--- src/components/Dashboard/DetailBusinessComponent.js
+++ src/components/Dashboard/DetailBusinessComponent.js
@@ -3,8 +3,33 @@ import PropTypes from 'prop-types';
 import DashboardTitle from './../common/DashboardTitle';
 
 const DetailBusinessComponent = props => {
+    const userId = sessionStorage.getItem('userId');
+
     return (
         <div className="container-fluid">
+            {props.deleteStatus === 204 ? (
+                <div>
+                    {props.deleteMsg &&
+                    <div className="alert alert-success" role="alert">
+                        {props.deleteMsg}
+                        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    }
+                </div>
+            ) : (
+                <div>
+                    {props.deleteMsg &&
+                    <div className="alert alert-danger" role="alert">
+                        {props.deleteMsg}
+                        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    }
+                </div>
+            )}
             <DashboardTitle title={props.name}/>
             <div className="row no-gutters">
                 <div className="col-md-12">
@@ -13,11 +38,14 @@ const DetailBusinessComponent = props => {
                             <span className="badge badge-info">{props.category}</span>
                             <span className="business-text"><span className="fa fa-map-marker"></span> {props.location}</span> &bullet;
                             <span className="business-text"><span className="fa fa-comments"></span> 4</span> &bullet;
-                            <span>By <span className="business-text">Business owner</span></span>
+                            <span>By <span className="business-text">{props.ownerName}</span></span>
                         </div>
                         <p>
                             {props.summary}
                         </p>
+                    </div>
+                    <div className="business-btn">
+                        <button type="button" className="btn btn-danger" onClick={props.onDelete}>Delete</button>
                     </div>
                 </div>
             </div>
@@ -63,9 +91,14 @@ const DetailBusinessComponent = props => {
 };
 
 DetailBusinessComponent.propTypes = {
+    'canDelete': PropTypes.bool,
     'category': PropTypes.string,
+    'deleteMsg': PropTypes.string,
+    'deleteStatus': PropTypes.number,
     'location': PropTypes.string,
     'name': PropTypes.string,
+    'onDelete': PropTypes.func,
+    'ownerName': PropTypes.string,
     'summary': PropTypes.string
 };
 
