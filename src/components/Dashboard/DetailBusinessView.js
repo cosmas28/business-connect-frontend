53893b9452f93be01e6de5d3d86f825e04eb7263
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-17 11:49:44 +0300

    feat: map deletBusiness state and dispatch to props in DetailBusinessView
    - ensures the deleteBusiness action creator can be accessed
    - ensures businessDelete state can be accessed
    
    [#156530746]

--- src/components/Dashboard/DetailBusinessView.js
+++ src/components/Dashboard/DetailBusinessView.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import DetailBusinessComponent from './DetailBusinessComponent';
 import DashboardNavBar from './../common/DashboardNavBar';
-import { fetchBusinessesById } from '../../actions/businessActions';
+import { fetchBusinessesById, deleteBusiness } from '../../actions/businessActions';
 
 
 class DetailBusinessView extends React.Component {
     constructor(props) {
         super(props);
         this.accessToken = sessionStorage.getItem('accessToken');
+        this.userId = sessionStorage.getItem('userId');
         this.businessId = this.props.match.params.id;
+        this.handleDeleteBusiness = this.handleDeleteBusiness.bind(this);
+    }
+
+    handleDeleteBusiness() {
+        this.props.deleteBusiness(this.accessToken, this.businessId);
     }
 
     componentDidMount() {
@@ -17,14 +23,24 @@ class DetailBusinessView extends React.Component {
     }
 
     render () {
+        let canDelete = false;
+        if (this.userId === this.props.business.owner_id) {
+            canDelete = true;
+        }
+
         return (
             <div className="page-wrapper">
                 <DashboardNavBar/>
                 <main className="main-body">
                     <DetailBusinessComponent
                         category={this.props.business.category}
+                        ownerName={this.props.business.created_by}
+                        canDelete={canDelete}
+                        deleteMsg={this.props.deleteRes.response_message}
+                        deleteStatus={this.props.deleteRes.status_code}
                         location={this.props.business.location}
                         name={this.props.business.name}
+                        onDelete={this.handleDeleteBusiness}
                         summary={this.props.business.summary}
                     />
                 </main>
@@ -34,11 +50,17 @@ class DetailBusinessView extends React.Component {
 }
 
 const mapStateToProps = state => {
-    return { business: state.business };
+    return {
+        business: state.business,
+        deleteRes: state.businessDelete
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return { fetchBusinessesById: (accessToken, id) => dispatch(fetchBusinessesById(accessToken, id)) };
+    return {
+        deleteBusiness: (accessToken, id) => dispatch(deleteBusiness(accessToken, id)),
+        fetchBusinessesById: (accessToken, id) => dispatch(fetchBusinessesById(accessToken, id))
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DetailBusinessView);
