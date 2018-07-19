844c80195be1b41128f6e64d0d68b19a09442f18
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-19 13:37:39 +0300

    fix: remove duplicate actions and reducers
    - ensures the user is redirected to the same business after adding a review
    
    [#156531115]

--- src/actions/reviewsActions.js
+++ src/actions/reviewsActions.js
@@ -1,22 +1,15 @@
 import axios from 'axios';
-import * as actionTypes from './actionTypes';
 import { addResponseMessage } from './responseMessage';
+import history from '../helpers/history';
 
 const apiUrl = 'http://127.0.0.1:5000/api/v2/businesses';
-
-export const addReviewsSuccess = (review) => {
-    return {
-        review,
-        type: actionTypes.ADD_REVIEWS_SUCCESS
-    };
-};
 
 export const addReviews = (accessToken, businessId, inputData) => {
     return (dispatch) => {
         return axios.post(apiUrl + '/' + businessId + '/reviews', inputData, { 'headers': { 'Authorization': `Bearer ${accessToken}` } })
         .then(response => {
             if (response.status === 200) {
-                dispatch(addReviewsSuccess(response.data));
+                history.push('/businesses/' + businessId);
             } else {
                 dispatch(addResponseMessage(response.data));
             }
