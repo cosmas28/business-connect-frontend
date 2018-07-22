33129ab66afb2bf7df341cc57c7e7031d45d95d0
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-07-22 13:48:07 +0300

    fix merge conflicts by accepting current changes

--- test/actions/reviewsAction.test.js
+++ test/actions/reviewsAction.test.js
@@ -22,52 +22,6 @@ describe('reviews tests', () => {
     const mockFetchedReviews = [];
 
     describe('add reviews tests', () => {
-        it('should return review and action type', () => {
-            expect(actions.addReviewsSuccess({
-                created_by: 'cosmas',
-                id: 1,
-                review: 'this business is very far from Kitale town!'
-            })).toEqual({
-                review: {
-                    created_by: 'cosmas',
-                    id: 1,
-                    review: 'this business is very far from Kitale town!'
-                },
-                type: types.ADD_REVIEWS_SUCCESS
-            });
-        });
-
-        it('should create ADD_REVIEWS_SUCCESS action', done => {
-            moxios.wait(() => {
-                const request = moxios.requests.mostRecent();
-                request.respondWith({
-                    response: {
-                        created_by: 'cosmas',
-                        id: 1,
-                        review: 'this business is very far from Kitale town!'
-                    },
-                    status: 200
-                });
-            });
-            const expectedAction = [
-                {
-                    review: {
-                        created_by: 'cosmas',
-                        id: 1,
-                        review: 'this business is very far from Kitale town!'
-                    },
-                    type: types.ADD_REVIEWS_SUCCESS
-                }
-            ];
-
-            const store = mockStore({ reviews: [] });
-
-            return store.dispatch(actions.addReviews(mockAccessToken, mockBusinesssId, mockData)).then(() => {
-                expect(store.getActions()).toEqual(expectedAction);
-                done();
-            });
-        });
-
         it('should create ADD_RESPONSE_MESSAGE action', done => {
             moxios.wait(() => {
                 const request = moxios.requests.mostRecent();
